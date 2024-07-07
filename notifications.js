a6cd1be925d6173a4126e63d92235dbe12fd965b
mycomport1 <111806665+mycomport1@users.noreply.github.com>
2024-07-07 18:03:39 +0200

    improve readability

--- notifications.js
+++ notifications.js
@@ -1,16 +1,21 @@
 require('dotenv').config();
 const nodemailer = require('nodemailer');
 
-const { EMAIL_HOST, EMAIL_PORT, EMAIL_USER, EMAIL_PASS } = process.env;
+const {
+  EMAIL_HOST,
+  EMAIL_PORT,
+  EMAIL_USER,
+  EMAIL_PASS
+} = process.env;
 
 const emailTransporter = nodemailer.createTransport({
   host: EMAIL_HOST,
   port: EMAIL_PORT,
-  secure: false, // Consider setting this to true if you are using the default port for secure SMTP (465)
+  secure: false, // Consider setting this to true if you're using port 465 for secure SMTP
   auth: {
     user: EMAIL_USER,
-    pass: EMAIL_PASS,
-  },
+    pass: EMAIL_PASS
+  }
 });
 
 const sendEmail = async (recipientEmail, emailSubject, emailBody) => {
@@ -18,10 +23,11 @@ const sendEmail = async (recipientEmail, emailSubject, emailBody) => {
     from: `"MindfulJournalApp" <${EMAIL_USER}>`,
     to: recipientEmail,
     subject: emailSubject,
-    text: emailBody,
+    text: emailBody
   };
+
   try {
-    await emailTransporter.sendMail(emailOptions);
+    await emailTransportation.sendMail(emailOptions);
     console.log('Email sent successfully');
   } catch (error) {
     console.error('Failed to send email:', error);
@@ -29,21 +35,17 @@ const sendEmail = async (recipientEmail, emailSubject, emailBody) => {
 };
 
 const remindJournalEntry = recipientEmail => {
-  sendEmail(
-    recipientEmail,
-    'Journal Entry Reminder',
-    'Hey there! Just a gentle reminder to make your journal entry for today. Take a moment to reflect and jot down your thoughts.',
-  );
+  sendEmail(recipientEmail,
+            'Journal Entry Reminder',
+            'Hey there! Just a gentle reminder to make your journal entry for today. Take a moment to reflect and jot down your thoughts.');
 };
 
 const promptMoodTracking = recipientEmail => {
-  sendEmail(
-    recipientEmail,
-    'How are you feeling today?',
-    "Don’t forget to track your mood today. It helps to see patterns over time and manage your well-being better.",
-  );
+  sendEmail(recipientEmail,
+            'How are you feeling today?',
+            "Don’t forget to track your mood today. It helps to see patterns over time and manage your well-being better.");
 };
 
 const sendNotificationWithEmail = (recipientEmail, notificationSubject, notificationMessage) => {
-  sendEmail(recipientEmail, notificationSubject, notificationMessage);
+  sendNewsEmail(recipientEmail, notificationSubject, notificationAssignment);
 };
